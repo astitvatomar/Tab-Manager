f27dd1131e6f7aa029d4f749f48bd380951b9906
astitvatomar <95633769+astitvatomar@users.noreply.github.com>
2024-10-01 18:03:58 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,9 +1,13 @@
 // Send message to background script to remove duplicate tabs
-document.getElementById("removeDuplicatesBtn").addEventListener("click", function () {
+document
+  .getElementById("removeDuplicatesBtn")
+  .addEventListener("click", function () {
     chrome.runtime.sendMessage({ action: "removeDuplicates" });
   });
-  
-  // Send message to background script to group tabs by domain
-  document.getElementById("groupByDomainBtn").addEventListener("click", function () {
+
+// Send message to background script to group tabs by domain
+document
+  .getElementById("groupByDomainBtn")
+  .addEventListener("click", function () {
     chrome.runtime.sendMessage({ action: "groupByDomain" });
   });
